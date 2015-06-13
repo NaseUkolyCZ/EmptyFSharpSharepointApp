45b7e8c2b56cd2ca86bb50bd5eeefe793dd55a15
David Podhola <david.podhola@naseukoly.cz>
2015-06-13 10:41:24 +0200

    Owin added and working

--- EmptyFSharpSharepointApp/Startup.fs
+++ EmptyFSharpSharepointApp/Startup.fs
@@ -1,6 +1,12 @@
-﻿open Owin
+﻿namespace Server
+
+open Owin
+open Microsoft.Owin
  
 type Startup() =
     member this.Configuration(app: Owin.IAppBuilder) =
         app.MapSignalR() |> ignore
         ()
+
+[<assembly: OwinStartup(typeof<Startup>)>]
+do ()
