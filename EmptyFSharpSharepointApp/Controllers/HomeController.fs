35dbe75465a4052bef965336242122f38a750117
David Podhola <david.podhola@naseukoly.cz>
2015-06-13 13:01:12 +0200

    mainscript

--- EmptyFSharpSharepointApp/Controllers/HomeController.fs
+++ EmptyFSharpSharepointApp/Controllers/HomeController.fs
@@ -10,4 +10,6 @@ open System.Web.Mvc.Ajax
 type HomeController() =
     inherit Controller()
     member this.Index () = this.View()
-
+ 
+    member this.MainScript() =
+        FunScript.Compiler.Compiler.Compile(<@ ScriptModule.Main() @>)
